fd014f3da4e257d7ab2bc612b981695e660d8901
Aseem <aseemanand2@gmail.com>
2020-11-11 22:39:30 +0530

    [Aseem] Created new repo and added files

---
+++ EmployeePayroll-MultiThreading/Program.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Aseem Anand"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace EmployeePayroll_MultiThreading
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ StoredProcedures/dbo.SpAddEmployeeDetails.sql
@@ -0,0 +1,17 @@
+﻿create procedure dbo.SpAddEmployeeDetails
+(	
+	@name	varchar(150),
+	@start	date,					
+	@gender	char(1),	
+	@phone_number	int,		
+	@address	varchar(150),
+	@department	varchar(150),
+	@Basic_Pay float,		
+	@Deductions	float,	
+	@Taxable_pay float,	
+	@Income_tax	float,				
+	@Net_pay	float		
+	)
+	as begin
+	Insert into employee_payroll values(@name,@start,@gender,@phone_number,@address,@department,@Basic_Pay,@Deductions,@Taxable_pay,@Income_tax,@Net_pay)
+	End
