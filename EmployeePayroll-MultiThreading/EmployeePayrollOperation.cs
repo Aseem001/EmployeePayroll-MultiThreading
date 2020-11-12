788c036bb496d427808fd3423add7573f904f769
Aseem <aseemanand2@gmail.com>
2020-11-12 16:07:48 +0530

    [Aseem] Added code to add employees into DB using threads in synchronization

--- EmployeePayroll-MultiThreading/EmployeePayrollOperation.cs
+++ EmployeePayroll-MultiThreading/EmployeePayrollOperation.cs
@@ -19,6 +19,12 @@ namespace EmployeePayroll_MultiThreading
         public static SqlConnection connection { get; set; }
 
         /// <summary>
+        /// Mutex is mutual exclusion used to synchronise the threads
+        /// Similar to locks
+        /// </summary>
+        private static Mutex mutex = new Mutex();
+
+        /// <summary>
         /// UC 1 : Adds the employee list to data base.
         /// </summary>
         /// <param name="employeeList">The employee list.</param>
@@ -45,14 +51,42 @@ namespace EmployeePayroll_MultiThreading
             {
                 //For each employeeData present in list new thread is created and all threads run according
                 //to the time slot assigned by the thread scheduler
-                Task thread = new Task(() =>
+                Thread thread = new Thread(() =>
                 {
                     Console.WriteLine("Current thread id: "+Thread.CurrentThread.ManagedThreadId);
                     Console.WriteLine("Employee Being added" + employeeData.EmployeeName);
-                    this.AddEmployeeToDatabase(employeeData);
+                    AddEmployeeToDatabase(employeeData);                    
                     Console.WriteLine("Employee added:" + employeeData.EmployeeName);
                 });
                 thread.Start();
+            });
+            Console.WriteLine("Total added employees: "+employeeList.Count);
+        }
+
+        /// <summary>
+        /// UC 3 : Adds the employee list to data base with synchronization.
+        /// </summary>
+        /// <param name="employeeList">The employee list.</param>
+        public void AddEmployeeListToDataBaseWithSynchronization(List<EmployeeModel> employeeList)
+        {            
+            /// Iterating over all the employee model list and point individual employee detail of the list
+            employeeList.ForEach(employeeData =>
+            {
+                Task thread = new Task(() =>
+                {
+                    //Lock the set of codes for the current employeeData
+                    lock (employeeData)
+                    {
+                        //mutex.WaitOne();
+                        Console.WriteLine("Current thread id: " + Thread.CurrentThread.ManagedThreadId);
+                        Console.WriteLine("Employee Being added" + employeeData.EmployeeName);
+                        this.AddEmployeeToDatabase(employeeData);
+                        Console.WriteLine("Employee added:" + employeeData.EmployeeName);
+                        //mutex.ReleaseMutex();
+                    }
+                });
+                thread.Start();
+                thread.Wait();
             });
         }
 
@@ -64,44 +98,48 @@ namespace EmployeePayroll_MultiThreading
         /// <exception cref="Exception"></exception>
         public bool AddEmployeeToDatabase(EmployeeModel employee)
         {
-            DBConnection dbc = new DBConnection();
-            connection = dbc.GetConnection();
-            try
+            lock(this)
             {
-                using (connection)
+                DBConnection dbc = new DBConnection();
+                connection = dbc.GetConnection();
+                try
                 {
-                    SqlCommand command = new SqlCommand("dbo.SpAddEmployeeDetails", connection);
-                    command.CommandType = CommandType.StoredProcedure;
-                    command.Parameters.AddWithValue("@name", employee.EmployeeName);
-                    command.Parameters.AddWithValue("@start", employee.StartDate);
-                    command.Parameters.AddWithValue("@gender", employee.Gender);
-                    command.Parameters.AddWithValue("@phone_number", employee.PhoneNumber);
-                    command.Parameters.AddWithValue("@address", employee.Address);
-                    command.Parameters.AddWithValue("@department", employee.Department);
-                    command.Parameters.AddWithValue("@Basic_Pay", employee.BasicPay);
-                    command.Parameters.AddWithValue("@Deductions", employee.Deductions);
-                    command.Parameters.AddWithValue("@Taxable_pay", employee.TaxablePay);
-                    command.Parameters.AddWithValue("@Income_tax", employee.Tax);
-                    command.Parameters.AddWithValue("@Net_pay", employee.NetPay);
-                    connection.Open();
-                    var result = command.ExecuteNonQuery();
-                    connection.Close();
-                    if (result != 0)
+                    using (connection)
                     {
-                        return true;
+                        SqlCommand command = new SqlCommand("dbo.SpAddEmployeeDetails", connection);
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@name", employee.EmployeeName);
+                        command.Parameters.AddWithValue("@start", employee.StartDate);
+                        command.Parameters.AddWithValue("@gender", employee.Gender);
+                        command.Parameters.AddWithValue("@phone_number", employee.PhoneNumber);
+                        command.Parameters.AddWithValue("@address", employee.Address);
+                        command.Parameters.AddWithValue("@department", employee.Department);
+                        command.Parameters.AddWithValue("@Basic_Pay", employee.BasicPay);
+                        command.Parameters.AddWithValue("@Deductions", employee.Deductions);
+                        command.Parameters.AddWithValue("@Taxable_pay", employee.TaxablePay);
+                        command.Parameters.AddWithValue("@Income_tax", employee.Tax);
+                        command.Parameters.AddWithValue("@Net_pay", employee.NetPay);
+                        connection.Open();
+                        var result = command.ExecuteNonQuery();
+                        connection.Close();
+                        if (result != 0)
+                        {
+                            return true;
+                        }
+                        return false;
                     }
-                    return false;
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+                finally
+                {
+                    if (connection.State.Equals("Open"))
+                        connection.Close();
                 }
             }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-            finally
-            {
-                if (connection.State.Equals("Open"))
-                    connection.Close();
-            }
+            
         }
     }
 }
